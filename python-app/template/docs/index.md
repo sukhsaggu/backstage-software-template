9969210020d95613a81761fc2b4620de21c476b6
Sukh Saggu <sukh.saggu@sunlife.com>
2025-06-24 07:41:23 -0400

    add python app software template

---
+++ python-app/template/docs/index.md
@@ -0,0 +1,9 @@
+# Documents for ${{values.app_name}}
+
+This application has two endpoints:
+- `python-app.test.com/api/v1/info`
+- `python-app.test.com/api/v1/healthz`
+
+# More details
+
+You can access the app by going to this url: `${{values.app_name}}-${{values.app_env}}.test.com/api/v1/healthz`
---
+++ python-app/template/src/app.py
@@ -0,0 +1,39 @@
+import datetime
+from flask import Flask, jsonify
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'name': 'Flask API',
+        'version': '1.0',
+        'description': 'A simple Flask API',
+        'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        'hostname': socket.gethostname(),
+        'message': 'Hello, World!!',
+        'deployed_on': 'k8s',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({
+        'name': 'Flask API',
+        'version': '1.0',
+        'description': 'A simple Flask API',
+        'status': 'up'
+
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+
+
+# '/api/v1/details'
+# 'api/v1/healthz'
